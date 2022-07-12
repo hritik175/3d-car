8cbe8c6e664d60b73d98206217bc58bedb761f17
hritik175 <hritikdalal.175@gmail.com>
2022-07-12 23:38:46 +0530

    added Orbitcontrols to the car

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 /**
  * SCENE
@@ -20,21 +20,24 @@ directionalLight.position.set(200, 500, 300)
 scene.add(directionalLight)
 
 
+
 /**
  * CAMERA
  */
 // Setting up camera
 const aspectRatio = window.innerWidth/ window.innerHeight
-const cameraWidth = 150
-const cameraHeight = cameraWidth / aspectRatio
+// const cameraWidth = 150
+// const cameraHeight = cameraWidth / aspectRatio
 
-const camera = new THREE.OrthographicCamera(
-    cameraWidth / -2,
-    cameraWidth / 2,
-    cameraWidth / 2,
-    cameraWidth / -2,0 ,1000
-)
-camera.position.set(200,200,200)
+// const camera = new THREE.OrthographicCamera(
+//     cameraWidth / -2,
+//     cameraWidth / 2,
+//     cameraWidth / 2,
+//     cameraWidth / -2,0 ,1000
+// )
+// camera.position.set(200,200,200)
+
+const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 1000)
 camera.lookAt(0,15,0)
 scene.add(camera)
 
@@ -64,7 +67,7 @@ const getCarSideTexture = () => {
     const context = canvas.getContext('2d')
 
     context.fillStyle = "#ffffff"
-    context.fillRect(0,0,128,32)
+    context.fillRect(0,0,0,20)
     context.fillStyle = "#666666"
     context.fillRect(10,8,38,24)
     context.fillRect(58,8,60,24)
@@ -76,9 +79,10 @@ const getCarSideTexture = () => {
  * GEOMETRY
  */
  const createWheels = () =>{
-    const geometry = new THREE.BoxBufferGeometry(12,25,32)
+    const geometry = new THREE.CylinderBufferGeometry(5,5,80,32)
     const material = new THREE.MeshLambertMaterial({ color: 0x333333 })
     const wheel = new THREE.Mesh(geometry, material)
+    wheel.rotation.x = 45
     return wheel;
 }
 
@@ -87,16 +91,16 @@ const createCar = ()=> {
     const car = new THREE.Group()
     const frontWheel = createWheels()
     frontWheel.position.y = 6
-    frontWheel.position.x = 18
+    frontWheel.position.x = 22
     car.add(frontWheel)
 
     const rearWheel = createWheels()
     rearWheel.position.y = 6
-    rearWheel.position.x = -18
+    rearWheel.position.x = -24
     car.add(rearWheel)
 
     const body = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(60,25,30),
+        new THREE.BoxBufferGeometry(80,20,40),
         new THREE.MeshLambertMaterial({ color: 0x78b14b})
     )
     body.position.y = 12
@@ -113,7 +117,7 @@ const createCar = ()=> {
 
 
     const cabin = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(30,30,28),[
+        new THREE.BoxBufferGeometry(40,15,35),[
         new THREE.MeshLambertMaterial({ map: carFrontTexture}),
         new THREE.MeshLambertMaterial({ map: carFrontTexture}),
         new THREE.MeshLambertMaterial({ color: 0xffffff}),
@@ -122,7 +126,7 @@ const createCar = ()=> {
         new THREE.MeshLambertMaterial({ map: leftSideTexture})
     ]
     )
-    cabin.position.y =35
+    cabin.position.y =28
     cabin.position.x= -8
     car.add(cabin)
     return car
@@ -134,15 +138,29 @@ scene.add(maruti)
 
 
 
-
-
 /**
  * RENDERER
  */
 const renderer = new THREE.WebGLRenderer({ antialias:true });
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
-
 document.body.appendChild(renderer.domElement);
 
+/**
+ * CONTROLS
+ */
+ const control = new OrbitControls(camera, renderer.domElement)
+ control.enableDamping = true
 
+ camera.position.set(200,200,200);
+ control.update()
+/**
+ * ANIMATION
+ */
+const animate = () =>{
+    
+    control.update()
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(animate)
+}
+animate();  
